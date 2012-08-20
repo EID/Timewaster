9bdbdbfc582664e9241fd3662bb83ded6294a186
CMat <mathieu0carrie@gmail.com>
2012-08-21 01:08:23 +0200

    Ajax score saving [unfinished]

--- class/main.class.php
+++ class/main.class.php
@@ -8,7 +8,49 @@ class Main
 		$this->dbh = $conf['dbh'];
 	}
 
-	// Put your methods here
+	public function save($pseudo, $score) {
+		$pseudo = htmlspecialchars($pseudo, ENT_QUOTES );
+		$score 	= intval($score);
+		
+		if( $this->usrExists($pseudo) ) {
+			$usrData = $this->get($pseudo);
+			
+			if ($score > $usrData['usr_score']) {
+				$q = $this->dbh->prepare('UPDATE tw_user SET usr_score=:score WHERE usr_pseudo=:pseudo');
+			}
+		} else {
+			$q = $this->dbh->prepare("INSERT INTO tw_user VALUES('', :pseudo, :score, 0)");
+		}
+		
+		if (isset($q)) {
+			$q->execute(array(
+				pseudo 	=> $pseudo,
+				score 	=> $score,
+			));
+		}
+	}
+	
+	public function usrExists($pseudo) {
+		$query = $this->dbh->prepare('SELECT COUNT(id) as nb FROM tw_user WHERE usr_pseudo=:pseudo');
+		$query->execute(array(
+			pseudo => $pseudo,
+		));
+		
+		$data = $query->fetch(PDO::FETCH_ASSOC);
+		
+		return $data['nb'] != 0;
+	}
+	
+	public function get($pseudo) {
+		$query = $this->dbh->prepare('SELECT * FROM tw_user WHERE usr_pseudo=:pseudo');
+		$query->execute(array(
+			pseudo => $pseudo,
+		));
+		
+		$data = $query->fetch(PDO::FETCH_ASSOC);
+		
+		return $data;
+	}
 }
 
 ?>
